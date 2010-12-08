157c370155db28203bd45141742b2ba46ab5f0cb
Pocket7878 <masa007>
2010-12-08 22:34:00 +0900

    add save.h

---
+++ save.c
@@ -0,0 +1,21 @@
+#include "save.h"
+
+int on(CELLP* p)
+{
+     cellpp[cellpptop] = p;
+     return cellpptop++;
+}
+
+void off(int i)
+{
+     cellpptop = i;
+}
+
+void gc_aux(int n, int a)
+{
+     int i;
+     printf("GC_AUX start**\n");
+     for(i = 0; i < cellpptop; i++) {
+	  Copying(cellpp[i], n, a);
+     }
+}
